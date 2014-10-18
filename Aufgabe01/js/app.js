dd2085b628f0a05bc069b1e79d588984d5de48b4
Rakatak <robin-steller@web.de>
2014-10-18 17:04:58 +0200

    finished all tasks plus bonus

--- Aufgabe01/js/app.js
+++ Aufgabe01/js/app.js
@@ -1,3 +1,38 @@
 /**
  * @author Robin Steller
  */
+ window.onload = function (){
+	 	 consoleMsg()
+
+ var invertBool = false;
+ var blurBool = false;
+ 
+ function consoleMsg() {
+	 console.log("Hello MME2, How you doin'?");
+ }
+ 
+
+ 
+ 
+  document.getElementById("invertBut").onclick=function(){	  
+	  if (invertBool == false) {
+		  	 document.getElementById("amon").className = "invert";
+			 invertBool = true;
+	 	 } else {
+		  	 document.getElementById("amon").className = "video";
+			 invertBool = false;
+		 }
+	};
+	
+	
+	 document.getElementById("blurBut").onclick=function(){	  
+	  if (invertBool == false) {
+		  	 document.getElementById("amon").className = "blur";
+			 invertBool = true;
+	 	 } else {
+		  	 document.getElementById("amon").className = "video";
+			 invertBool = false;
+		 }
+	};
+  
+ }
